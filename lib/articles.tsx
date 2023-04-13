62b544729173667c723f462c1befec859fcd0e6f
huayemao <huayemao4g@gmail.com>
2023-04-13 22:33:30 +0800

    feat: 获取 Post 时带上 tags

--- lib/articles.tsx
+++ lib/articles.tsx
@@ -2,14 +2,25 @@ import prisma from "@/prisma/client";
 import { cache } from "react";
 import { markdownExcerpt, markdownToHtml } from "./utils";
 
-export const getArticle = cache(
-  async (id: number) =>
-    await prisma.articles.findUnique({
-      where: {
-        id: id,
+export const getArticle = cache(async (id: number) => {
+  const res = await prisma.articles.findUnique({
+    where: {
+      id: id,
+    },
+    include: {
+      tags_articles_links: {
+        include: {
+          tags: true,
+        },
       },
-    })
-);
+    },
+  });
+
+  return {
+    ...res,
+    tags: res?.tags_articles_links.map((e) => e.tags),
+  };
+});
 
 export const getArticles = cache(
   async () =>
@@ -19,12 +30,20 @@ export const getArticles = cache(
           orderBy: {
             updated_at: "desc",
           },
+          include: {
+            tags_articles_links: {
+              include: {
+                tags: true,
+              },
+            },
+          },
         })
       ).map(async (e) => {
         return {
           ...e,
           content: await markdownToHtml(e.content),
           excerpt: await markdownExcerpt(e.content),
+          tags: e.tags_articles_links.map((e) => e.tags),
         };
       })
     )
