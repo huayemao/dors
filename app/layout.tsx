eba3394a5a833fb99396d1be7073b77879ad8b78
huayemao <huayemao4g@gmail.com>
2023-06-21 15:37:33 +0800

    fix: 使用 next/head 代替 head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { Metadata } from "next";
+import Head from "next/head";
 import Script from "next/script";
 
 export const metadata: Metadata = {
@@ -16,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head>
+      <Head>
         <Script strategy="beforeInteractive">
           {`if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
   document.documentElement.classList.add('dark')
@@ -25,7 +26,7 @@ export default function RootLayout({
 }`}
         </Script>
         <Script src="http://qzonestyle.gtimg.cn/qzone/qzact/common/share/share.js"></Script>
-      </head>
+      </Head>
       <body>{children}</body>
     </html>
   );
