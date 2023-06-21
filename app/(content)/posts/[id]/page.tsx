8568b4249f6058e4892d9bc541c8cd73263ce6ab
huayemao <huayemao4g@gmail.com>
2023-06-21 15:56:19 +0800

    fix: ShareButton client comp

--- app/(content)/posts/[id]/page.tsx
+++ app/(content)/posts/[id]/page.tsx
@@ -12,6 +12,7 @@ import Link from "next/link";
 import { join } from "path";
 import rehypeRaw from "rehype-raw";
 import remarkShikiTwoslash from "remark-shiki-twoslash";
+import { ShareButton } from "../../../../components/ShareButton";
 const theme = require("shiki/themes/nord.json");
 
 export const revalidate = 600;
@@ -65,14 +66,6 @@ async function page({ params }) {
   const url = await getBase64Image(coverImage);
 
   const excerpt = (await markdownExcerpt(article?.content || "")) + "...";
-
-  const share = () => {
-    /* @ts-ignore */
-    window.setShareInfo({
-      title: article.title,
-      url: window.location.href,
-    });
-  };
 
   return (
     <main className="w-full">
@@ -221,44 +214,12 @@ async function page({ params }) {
                     </h3>
 
                     <div className="flex gap-4">
-                      <button
-                        onClick={share}
-                        className="
-              flex-1
-              inline-flex
-              justify-center
-              items-center
-              py-4
-              px-5
-              rounded
-              bg-muted-200
-              dark:bg-muted-700
-              hover:bg-muted-100
-              dark:hover:bg-muted-600
-              text-muted-600
-              dark:text-muted-400
-              transition-colors
-              duration-300
-              cursor-pointer
-              tw-accessibility
-            "
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          aria-hidden="true"
-                          role="img"
-                          width="0.63em"
-                          height="1em"
-                          viewBox="0 0 320 512"
-                          data-icon="fa6-brands:facebook-f"
-                          className="iconify w-4 h-4 iconify--fa6-brands"
-                        >
-                          <path
-                            fill="currentColor"
-                            d="m279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
-                          ></path>
-                        </svg>
-                      </button>
+                      <ShareButton
+                        options={{
+                          title: article.title,
+                        }}
+                      ></ShareButton>
+
                       <button
                         className="
               flex-1
@@ -293,80 +254,6 @@ async function page({ params }) {
                           <path
                             fill="currentColor"
                             d="M459.37 151.716c.325 4.548.325 9.097.325 13.645c0 138.72-105.583 298.558-298.558 298.558c-59.452 0-114.68-17.219-161.137-47.106c8.447.974 16.568 1.299 25.34 1.299c49.055 0 94.213-16.568 130.274-44.832c-46.132-.975-84.792-31.188-98.112-72.772c6.498.974 12.995 1.624 19.818 1.624c9.421 0 18.843-1.3 27.614-3.573c-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319c-28.264-18.843-46.781-51.005-46.781-87.391c0-19.492 5.197-37.36 14.294-52.954c51.655 63.675 129.3 105.258 216.365 109.807c-1.624-7.797-2.599-15.918-2.599-24.04c0-57.828 46.782-104.934 104.934-104.934c30.213 0 57.502 12.67 76.67 33.137c23.715-4.548 46.456-13.32 66.599-25.34c-7.798 24.366-24.366 44.833-46.132 57.827c21.117-2.273 41.584-8.122 60.426-16.243c-14.292 20.791-32.161 39.308-52.628 54.253z"
-                          ></path>
-                        </svg>
-                      </button>
-                      <button
-                        className="
-              flex-1
-              inline-flex
-              justify-center
-              items-center
-              py-4
-              px-5
-              rounded
-              bg-muted-200
-              dark:bg-muted-700
-              hover:bg-muted-100
-              dark:hover:bg-muted-600
-              text-muted-600
-              dark:text-muted-400
-              transition-colors
-              duration-300
-              cursor-pointer
-              tw-accessibility
-            "
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          aria-hidden="true"
-                          role="img"
-                          width="0.88em"
-                          height="1em"
-                          viewBox="0 0 448 512"
-                          data-icon="fa6-brands:linkedin-in"
-                          className="iconify w-4 h-4 iconify--fa6-brands"
-                        >
-                          <path
-                            fill="currentColor"
-                            d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2c-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3c94 0 111.28 61.9 111.28 142.3V448z"
-                          ></path>
-                        </svg>
-                      </button>
-                      <button
-                        className="
-              flex-1
-              inline-flex
-              justify-center
-              items-center
-              py-4
-              px-5
-              rounded
-              bg-muted-200
-              dark:bg-muted-700
-              hover:bg-muted-100
-              dark:hover:bg-muted-600
-              text-muted-600
-              dark:text-muted-400
-              transition-colors
-              duration-300
-              cursor-pointer
-              tw-accessibility
-            "
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          aria-hidden="true"
-                          role="img"
-                          width="1em"
-                          height="1em"
-                          viewBox="0 0 512 512"
-                          data-icon="fa6-solid:envelope"
-                          className="iconify w-4 h-4 iconify--fa6-solid"
-                        >
-                          <path
-                            fill="currentColor"
-                            d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4l217.6 163.2c11.4 8.5 27 8.5 38.4 0l217.6-163.2c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176v208c0 35.3 28.7 64 64 64h384c35.3 0 64-28.7 64-64V176L294.4 339.2a63.9 63.9 0 0 1-76.8 0L0 176z"
                           ></path>
                         </svg>
                       </button>
---
+++ components/ShareButton.tsx
@@ -0,0 +1,31 @@
+"use client";
+export const ShareButton = ({ options }) => {
+  const share = () => {
+    /* @ts-ignore */
+    window.setShareInfo({ ...options, url: window.location.href });
+  };
+
+  return (
+    <button
+      onClick={share}
+      className="flex-1 inline-flex justify-center items-center py-4 px-5 rounded bg-muted-200 dark:bg-muted-700 hover:bg-muted-100 dark:hover:bg-muted-600 text-muted-600 dark:text-muted-400 transition-colors duration-300 cursor-pointer tw-accessibility
+"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        role="img"
+        width="0.63em"
+        height="1em"
+        viewBox="0 0 320 512"
+        data-icon="fa6-brands:facebook-f"
+        className="iconify w-4 h-4 iconify--fa6-brands"
+      >
+        <path
+          fill="currentColor"
+          d="m279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
+        ></path>
+      </svg>
+    </button>
+  );
+};
