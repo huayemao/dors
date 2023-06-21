e1da971a19ef30cdfd97b8371c3ed52c1d6aa1dd
huayemao <huayemao4g@gmail.com>
2023-06-21 09:35:06 +0800

    style: 代码块隐藏语言

--- app/(content)/posts/[id]/page.tsx
+++ app/(content)/posts/[id]/page.tsx
@@ -187,7 +187,7 @@ async function page({ params }) {
                           d="m1.027 11.993l4.235 4.25L6.68 14.83l-1.821-1.828L22.974 13v-2l-18.12.002L6.69 9.174L5.277 7.757l-4.25 4.236Z"
                         ></path>
                       </svg>
-                      <span>Back to Blog</span>
+                      <span>返回</span>
                     </Link>
                   </div>
                   <article className="prose dark:prose-dark lg:prose-xl py-6">
