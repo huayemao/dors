c2567515320020a29f20287eae83128e7ece3523
huayemao <huayemao4g@gmail.com>
2023-06-29 10:58:52 +0800

    style: loading 组件居中

--- components/loading.tsx
+++ components/loading.tsx
@@ -2,7 +2,7 @@ import styles from "@/styles/loading.module.css";
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
-    <div className="w-full h-full flex items-center justify-center min-h-[80vh]">
+    <div className="fixed inset-0 flex items-center justify-center">
       <div className={styles.container}>
         <span className={styles.item}></span>
         <span className={styles.item}></span>
