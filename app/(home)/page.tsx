c4bfe7d7a2d95214a4b77d8195946c05c54a3b4b
huayemao <huayemao4g@gmail.com>
2023-08-01 09:04:00 +0800

    feat: 主页去掉 searchParams

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,12 +9,8 @@ type Posts = Awaited<ReturnType<typeof getProcessedPosts>>;
 // export const revalidate = 300;
 //https://beta.nextjs.org/docs/data-fetching/fetching#segment-cache-configuration
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: SearchParams;
-}) {
-  const posts = await getProcessedPosts(await getPosts(searchParams));
+export default async function Home({}: {}) {
+  const posts = await getProcessedPosts(await getPosts());
   const pageCount = await getPageCount();
 
   return (
