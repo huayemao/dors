b92d7ad946a325393ff51718e26f496bb277e0b7
huayemao <huayemao4g@gmail.com>
2023-09-08 16:55:29 +0800

    fix: 修复暗黑模式切换不灵敏的问题

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -20,11 +20,11 @@ export const Nav = () => {
     e
   ) => {
     const v = e.target.checked;
-    localStorage.theme = v ? "light" : "dark";
+    localStorage.setItem("theme", v ? "light" : "dark");
 
     if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
+      v ||
+      (!localStorage.getItem("theme") &&
         window.matchMedia("(prefers-color-scheme: dark)").matches)
     ) {
       document.documentElement.classList.add("dark");
@@ -37,7 +37,7 @@ export const Nav = () => {
   };
 
   useEffect(() => {
-    scrollHeader()
+    scrollHeader();
     window.addEventListener("scroll", scrollHeader);
 
     return () => {
@@ -1084,112 +1084,6 @@ export const Nav = () => {
               </a>
             </li>
           </ul> */}
-          <div className="flex flex-col my-3 lg:hidden">
-            <div className="mb-4">
-              <label
-                className="
-              block
-              w-9
-              h-9
-              overflow-hidden
-              relative
-              mx-auto
-              lg:m-0
-              rounded-full
-              focus-within:tw-accessibility-static
-            "
-              >
-                <input
-                  type="checkbox"
-                  className="absolute top-0 left-0 z-[2] w-full h-full opacity-0 cursor-pointer"
-                />
-                <span
-                  className="
-                block
-                relative
-                w-9
-                h-9
-                bg-white
-                dark:bg-slate-800
-                rounded-full
-                border border-slate-300
-                dark:border-slate-700
-              "
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlnsXlink="http://www.w3.org/1999/xlink"
-                    aria-hidden="true"
-                    role="img"
-                    className="block absolute top-1/2 left-1/2 w-5 h-5 text-yellow-400 transition-all duration-300 pointer-events-none opacity-100 -translate-x-[50%] -translate-y-1/2"
-                    width="32"
-                    height="32"
-                    preserveAspectRatio="xMidYMid meet"
-                    viewBox="0 0 24 24"
-                  >
-                    <g
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <circle
-                        cx="12"
-                        cy="12"
-                        r="5"
-                        className="fill-current"
-                      ></circle>
-                      <path className="fill-current" d="M12 1v2"></path>
-                      <path className="fill-current" d="M12 21v2"></path>
-                      <path
-                        className="fill-current"
-                        d="M4.22 4.22l1.42 1.42"
-                      ></path>
-                      <path
-                        className="fill-current"
-                        d="M18.36 18.36l1.42 1.42"
-                      ></path>
-                      <path className="fill-current" d="M1 12h2"></path>
-                      <path className="fill-current" d="M21 12h2"></path>
-                      <path
-                        className="fill-current"
-                        d="M4.22 19.78l1.42-1.42"
-                      ></path>
-                      <path
-                        className="fill-current"
-                        d="M18.36 5.64l1.42-1.42"
-                      ></path>
-                    </g>
-                  </svg>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlnsXlink="http://www.w3.org/1999/xlink"
-                    aria-hidden="true"
-                    role="img"
-                    className="block absolute top-1/2 left-1/2 w-5 h-5 text-yellow-400 transition-all duration-300 pointer-events-none opacity-0 -translate-x-[45%] -translate-y-[150%]"
-                    width="32"
-                    height="32"
-                    preserveAspectRatio="xMidYMid meet"
-                    viewBox="0 0 24 24"
-                  >
-                    <g
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path
-                        className="fill-current"
-                        d="M21 12.79A9 9 0 1 1 11.21 3A7 7 0 0 0 21 12.79z"
-                      ></path>
-                    </g>
-                  </svg>
-                </span>
-              </label>
-            </div>
-          </div>
         </div>
 
         <div className="hidden lg:flex lg:w-1/5 lg:justify-end lg:gap-x-4">
