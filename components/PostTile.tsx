7791eed02154b8a6abb5288b636fcc9e69379024
huayemao <huayemao4g@gmail.com>
2023-09-19 15:29:15 +0800

    style: 优化分类后的文章列表样式

--- components/PostTile.tsx
+++ components/PostTile.tsx
@@ -39,7 +39,7 @@ function PostTile({
     return (
       <li
         style={{ listStyle: "none" }}
-        className={cn({ "bg-white p-2 rounded-lg border": rounded })}
+        className={cn({ "bg-white p-2 rounded-lg border dark:bg-muted-800 dark:border-muted-700": rounded })}
       >
         <Link href={"/posts/" + id} className="flex items-center">
           <div className="relative flex justify-start gap-2 w-full">
@@ -52,7 +52,7 @@ function PostTile({
               quality={60}
             />
             <div>
-              <h3 className="font-heading font-medium text-muted-800 dark:text-muted-50 leading-snug overflow-hidden text-ellipsis max-w-3/4 line-clamp-2 mb-1">
+              <h3 className="truncate font-heading font-medium text-muted-800 dark:text-muted-50 leading-snug overflow-hidden text-ellipsis max-w-3/4 line-clamp-2 mb-1">
                 {post.title}
               </h3>
               <p className="font-sans text-sm text-muted-400">
