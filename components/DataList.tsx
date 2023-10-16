da97dbe404dc97317b8da1d2b4d0a77f71041222
huayemao <huayemao4g@gmail.com>
2023-10-16 16:52:18 +0800

    feat: 更新 DataList 样式

--- components/DataList.tsx
+++ components/DataList.tsx
@@ -5,16 +5,16 @@ type Props = {
 
 function DataList({ data, title }: Props) {
   return (
-    <div className="not-prose shadow rounded nui-card-white flex flex-col py-6 mb-4">
+    <div className="not-prose nui-card nui-card-rounded nui-card-white flex flex-col py-6">
       <div className="mb-6 flex items-center justify-between px-6">
-        <h3 className="font-semibold text-lg  leading-4 text-muted-800 dark:text-white">
+        <h3 className="nui-heading nui-heading-md nui-weight-semibold nui-lead-tight text-muted-800 dark:text-white">
           <span>{title}</span>
         </h3>
       </div>
       <div className="border-muted-200 dark:border-muted-700 space-y-5 px-6 ">
         {data.map((e) => (
           <div key={e.key} className="flex items-center justify-between">
-            <h5 className="font-medium leading-4 text-sm text-muted-800 dark:text-white">
+            <h5 className="nui-heading nui-heading-sm nui-weight-medium nui-lead-tight text-muted-800 dark:text-white">
               <span>{e.key}</span>
             </h5>
             <div className="flex items-center gap-1">
