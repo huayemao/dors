5951e1c30d78dfafb2f8aa3278206d20e022141e
huayemao <huayemao4g@gmail.com>
2023-10-16 16:01:33 +0800

    feat: 优化标签表单样式；升级 shuriken-ui

---
+++ app/admin/tags/TagForm.tsx
@@ -0,0 +1,81 @@
+"use client";
+import Input from "@/components/Base/Input";
+import { useEffect } from "react";
+import { Props } from "./page";
+
+export function TagForm({ id, isEditing, tags }: Props) {
+  useEffect(() => {
+    const nameInput = document.getElementById("name") as HTMLInputElement;
+    nameInput.addEventListener("input", (event) => {
+      if (tags.map((e) => e.name).includes(nameInput.value)) {
+        nameInput.setCustomValidity("该标签已存在！");
+      } else {
+        nameInput.setCustomValidity("");
+      }
+    });
+  }, [id]);
+
+  return (
+    <form
+      action={"/api/createTag"}
+      method="POST"
+      className="w-96 rounded  bg-white p-8 mx-auto space-y-4"
+      key={id}
+    >
+      <input
+        name="id"
+        id="id"
+        readOnly
+        hidden
+        disabled={!isEditing}
+        defaultValue={id}
+      />
+      <div className="flex items-center gap-4 ">
+        <div className="bg-primary-500/20 text-primary-500 flex h-14 w-14 shrink-0 items-center justify-center rounded-full font-sans text-2xl">
+          <svg
+            data-v-26e5b7b0=""
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            aria-hidden="true"
+            role="img"
+            className="icon h-5 w-5"
+            width="1em"
+            height="1em"
+            viewBox="0 0 256 256"
+          >
+            <g fill="currentColor">
+              <path
+                d="M216 48v40H40V48a8 8 0 0 1 8-8h160a8 8 0 0 1 8 8Z"
+                opacity=".2"
+              ></path>
+              <path d="M208 32h-24v-8a8 8 0 0 0-16 0v8H88v-8a8 8 0 0 0-16 0v8H48a16 16 0 0 0-16 16v160a16 16 0 0 0 16 16h160a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16ZM72 48v8a8 8 0 0 0 16 0v-8h80v8a8 8 0 0 0 16 0v-8h24v32H48V48Zm136 160H48V96h160v112Z"></path>
+            </g>
+          </svg>
+        </div>
+        <div className="block text-xl font-semibold text-gray-700">
+          <h3 className="leading-5 font-heading font-medium text-lg">
+            {isEditing ? "编辑" : "新建"}标签
+          </h3>
+          <p className="text-sm font-normal leading-5 text-muted-400">
+            创建新的标签
+          </p>
+        </div>
+      </div>
+      <Input
+        label="名称"
+        id="name"
+        required
+        defaultValue={tags.find((t) => t.id == id)?.name || ""}
+      />
+      <div className="w-full text-right">
+        {" "}
+        <button
+          type="submit"
+          className="nui-button nui-button-medium nui-button-curved nui-button-solid nui-button-primary !h-12 w-32"
+        >
+          确定
+        </button>
+      </div>
+    </form>
+  );
+}
--- app/admin/tags/page.tsx
+++ app/admin/tags/page.tsx
@@ -1,19 +1,20 @@
-import Input from "@/components/Base/Input";
 import { getTags } from "@/lib/tags";
 import Link from "next/link";
+import { TagForm } from "./TagForm";
 
-async function Tags() {
+async function Tags({ searchParams }) {
+  const { id } = searchParams;
+  const tagId = parseInt(id);
   const tags = await getTags();
-
-  const pattern = `^(?!.*(${tags.map((e) => e.name).join("|")})).*$`;
+  const isEditing = !Number.isNaN(tagId);
 
   return (
     <div className="space-y-8 p-8">
-      <div className="flex flex-wrap gap-4  justify-center max-w-3xl mx-auto">
+      <div className="border p-10 flex flex-wrap gap-4  justify-center max-w-3xl mx-auto">
         {tags.map(({ name, id }) => {
           return (
             <Link
-              href={`/tags/${id}`}
+              href={`/admin/tags?id=${id}`}
               key={id}
               className="font-sans py-1.5 px-3 rounded-xl text-sm text-muted-500 dark:text-muted-300 bg-white dark:bg-muted-800 border border-muted-200 dark:border-muted-700 shadow-lg shadow-muted-400/20 dark:shadow-muted-700/20"
             >
@@ -22,11 +23,18 @@ async function Tags() {
           );
         })}
       </div>
-      <form action="/api/createTag" method="POST" className="w-96 rounded shadow bg-white p-8 mx-auto">
-        <Input label="名称" id="name" pattern={pattern} required></Input>
-      </form>
+      {<TagForm id={id} isEditing={isEditing} tags={tags}></TagForm>}
     </div>
   );
 }
+
+export type Props = {
+  isEditing: boolean;
+  id: number;
+  tags: {
+    id: number;
+    name: string | null;
+  }[];
+};
 
 export default Tags;
