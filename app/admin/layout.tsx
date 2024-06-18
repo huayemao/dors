238a8f0a85544b09e8f772d921c39f3438c667a0
huayemao <huayemao4g@gmail.com>
2024-06-18 10:37:13 +0800

    feat: 编辑器工具栏置顶

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -12,7 +12,7 @@ export default async function AdminLayout({
       <div className="dark:bg-muted-800 border-muted-200 dark:border-muted-700 fixed start-0 top-0 z-[60] flex h-full flex-col border-r bg-white transition-all duration-300 w-[240px] -translate-x-full lg:translate-x-0">
         <AdminMenu />
       </div>
-      <div className="bg-muted-100 dark:bg-muted-900 relative min-h-screen w-full overflow-x-hidden px-4 transition-all duration-300 xl:px-10 lg:max-w-[calc(100%_-_240px)] lg:ms-[240px]">
+      <div className="bg-muted-100 dark:bg-muted-900 relative min-h-screen w-full px-4 transition-all duration-300 xl:px-10 lg:max-w-[calc(100%_-_240px)] lg:ms-[240px]">
         {children}
       </div>
     </div>
--- components/PostEditor.tsx
+++ components/PostEditor.tsx
@@ -12,7 +12,7 @@ import {
 } from "@shuriken-ui/react";
 import { Plus, Save, Settings, TimerReset } from "lucide-react";
 import Link from "next/link";
-import { FormEventHandler, useContext, useState } from "react";
+import { FormEventHandler, useContext, useEffect, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import { Modal } from "./Base/Modal";
 
@@ -68,13 +68,44 @@ export function PostEditor({ post }: PostEditorProps) {
   const [modalOpen, setModalOpen] = useState(false);
   const [action, setAction] = useState<"upload" | "unicode">("upload");
   const [content, setContent] = useState(post?.content || "");
+  const [pinned, setPinned] = useState(false);
 
   const categoryId = post?.posts_category_links[0]?.category_id;
 
+  useEffect(() => {
+    const el = document.querySelector(".toolbox") as HTMLDivElement;
+    const observer = new IntersectionObserver(
+      ([e]) => {
+        setPinned(e.intersectionRatio < 1);
+      },
+      { threshold: [1] }
+    );
+    observer.observe(el);
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <>
+      <div
+        className={cn(
+          "toolbox sticky right-0 left-0 top-[-1px] mb-6  transition-all",
+          { "p-4": pinned }
+        )}
+      >
+        <div
+          className={cn("flex items-center transition-all", {
+            "nui-card nui-card-shadow nui-card-rounded-md px-4 py-2 bg-white":
+              pinned,
+          })}
+        >
+          {CatSelect()}
+          <Action />
+        </div>
+      </div>
       <form
-        className="bg-white dark:bg-black  max-w-screen-lg"
+        className="bg-white dark:bg-black  max-w-screen-lg overflow-x-hidden"
         action={post ? "/api/updatePost" : "/api/createPost"}
         method="POST"
         onSubmit={handleOnSubmit}
@@ -89,95 +120,7 @@ export function PostEditor({ post }: PostEditorProps) {
           />
         )}
 
-        <div className="relative min-h-[500px] w-full border-stone-200 p-12 pt-16 px-8 dark:border-stone-700 sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:px-12 sm:shadow-lg">
-          <div className="absolute right-0 left-0 px-5 top-5 mb-6 flex items-center ">
-            {CatSelect()}
-            <div className="space-x-3 flex items-center">
-              {post && (
-                <>
-                  <BaseDropdown
-                    renderButton={() => (
-                      <BaseButtonIcon rounded="md" size="sm">
-                        <Plus className="h-4 w-4" />
-                      </BaseButtonIcon>
-                    )}
-                  >
-                    <BaseDropdownItem
-                      title="上传文件"
-                      onClick={() => {
-                        setAction("upload");
-                        setModalOpen(true);
-                      }}
-                    ></BaseDropdownItem>
-                    <BaseDropdownItem
-                      title="Unicode 表情"
-                      onClick={() => {
-                        setAction("unicode");
-                        setModalOpen(true);
-                      }}
-                    />
-                    <BaseDropdownItem
-                      title="最近文件"
-                      text="获取最近文件的 markdown 标记"
-                      onClick={() => {
-                        fetch("/api/files/getLatestFile")
-                          .then((res) => res.json())
-                          .then(
-                            (json) => `${SITE_META.url}/api/files/${json.name}`
-                          )
-                          .then(copyTextToClipboard)
-                          .then(() => {
-                            alert("已复制到剪切板");
-                          });
-                      }}
-                    ></BaseDropdownItem>
-                  </BaseDropdown>
-
-                  <Link
-                    legacyBehavior
-                    passHref
-                    href={`/admin/posts/${post.id}/settings`}
-                    className="flex items-center space-x-1 text-sm text-stone-400 hover:text-stone-500"
-                  >
-                    <BaseButtonIcon rounded="md" size="sm">
-                      <Settings className="h-4 w-4" />
-                    </BaseButtonIcon>
-                  </Link>
-                  <label className="text-stone-400 hover:text-stone-500">
-                    <div className="nui-button-icon nui-button-rounded-md nui-button-small nui-button-default">
-                      <TimerReset
-                        className={cn("h-4 w-4 cursor-pointer", {
-                          "text-primary-500": reserveUpdateTime,
-                        })}
-                      />
-                    </div>
-                    <input
-                      className="appearance-none m-0 bg-transparent hidden"
-                      type="checkbox"
-                      onChange={(e) => {
-                        setReserveUpdateTime(e.target.checked);
-                      }}
-                      defaultChecked={reserveUpdateTime}
-                    />
-                  </label>
-                  <input
-                    hidden
-                    disabled={!reserveUpdateTime}
-                    id="updated_at"
-                    name="updated_at"
-                    type="datetime-local"
-                    defaultValue={getDateForDateTimeInput(
-                      post?.updated_at as Date
-                    )}
-                  ></input>
-                </>
-              )}
-              <BaseButtonIcon rounded="md" type="submit" size="sm">
-                <Save className="h-4 w-4" />
-              </BaseButtonIcon>
-            </div>
-          </div>
-
+        <div className="min-h-[500px] w-full border-stone-200 p-12 pt-16 px-8 dark:border-stone-700 sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:px-12 sm:shadow-lg">
           <div className="mb-5 flex flex-col space-y-3 border-b border-stone-200 pb-5 dark:border-stone-700">
             <input
               id={"title"}
@@ -206,6 +149,7 @@ export function PostEditor({ post }: PostEditorProps) {
                 const thisEl = e.target as HTMLTextAreaElement;
                 setContent(thisEl.value);
               }}
+              value={content}
               defaultValue={post?.content || ""}
               data-original-value={post?.content}
               placeholder="正文"
@@ -225,6 +169,91 @@ export function PostEditor({ post }: PostEditorProps) {
       </Modal>
     </>
   );
+
+  function Action() {
+    return (
+      <div className="space-x-3 flex items-center">
+        {post && (
+          <>
+            <BaseDropdown
+              renderButton={() => (
+                <BaseButtonIcon rounded="md" size="sm">
+                  <Plus className="h-4 w-4" />
+                </BaseButtonIcon>
+              )}
+            >
+              <BaseDropdownItem
+                title="上传文件"
+                onClick={() => {
+                  setAction("upload");
+                  setModalOpen(true);
+                }}
+              ></BaseDropdownItem>
+              <BaseDropdownItem
+                title="Unicode 表情"
+                onClick={() => {
+                  setAction("unicode");
+                  setModalOpen(true);
+                }}
+              />
+              <BaseDropdownItem
+                title="最近文件"
+                text="获取最近文件的 markdown 标记"
+                onClick={() => {
+                  fetch("/api/files/getLatestFile")
+                    .then((res) => res.json())
+                    .then((json) => `${SITE_META.url}/api/files/${json.name}`)
+                    .then(copyTextToClipboard)
+                    .then(() => {
+                      alert("已复制到剪切板");
+                    });
+                }}
+              ></BaseDropdownItem>
+            </BaseDropdown>
+
+            <Link
+              legacyBehavior
+              passHref
+              href={`/admin/posts/${post.id}/settings`}
+              className="flex items-center space-x-1 text-sm text-stone-400 hover:text-stone-500"
+            >
+              <BaseButtonIcon rounded="md" size="sm">
+                <Settings className="h-4 w-4" />
+              </BaseButtonIcon>
+            </Link>
+            <label className="text-stone-400 hover:text-stone-500">
+              <div className="nui-button-icon nui-button-rounded-md nui-button-small nui-button-default">
+                <TimerReset
+                  className={cn("h-4 w-4 cursor-pointer", {
+                    "text-primary-500": reserveUpdateTime,
+                  })}
+                />
+              </div>
+              <input
+                className="appearance-none m-0 bg-transparent hidden"
+                type="checkbox"
+                onChange={(e) => {
+                  setReserveUpdateTime(e.target.checked);
+                }}
+                defaultChecked={reserveUpdateTime}
+              />
+            </label>
+            <input
+              hidden
+              disabled={!reserveUpdateTime}
+              id="updated_at"
+              name="updated_at"
+              type="datetime-local"
+              defaultValue={getDateForDateTimeInput(post?.updated_at as Date)}
+            ></input>
+          </>
+        )}
+        <BaseButtonIcon rounded="md" type="submit" size="sm">
+          <Save className="h-4 w-4" />
+        </BaseButtonIcon>
+      </div>
+    );
+  }
 
   function CatSelect() {
     const [v, setV] = useState(
