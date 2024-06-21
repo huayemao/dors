e464631d488f268a23d90551ba2efc3becdb134f
huayemao <huayemao4g@gmail.com>
2024-06-21 23:22:13 +0800

    feat: 优化文件列表页样式

--- app/admin/files/page.tsx
+++ app/admin/files/page.tsx
@@ -1,18 +1,51 @@
 import prisma from "@/lib/prisma";
+import {
+  BaseButtonIcon,
+  BaseCard,
+  BaseDropdown,
+  BaseDropdownItem,
+  BaseList,
+  BaseListItem,
+} from "@shuriken-ui/react";
+import { EllipsisIcon, FileIcon, TrashIcon } from "lucide-react";
 import { UploadForm } from "../../../components/UploadForm";
 
 export default async function UploadTest() {
-  const list = await prisma.file.findMany();
+  const list = await prisma.file.findMany({
+    select: {
+      id: true,
+      name: true,
+    },
+  });
 
   return (
-    <div className="py-4 pt-20 md:px-12">
-      {list.map((e) => (
-        <div key={e.id}>{e.name}</div>
-      ))}
-
+    <div>
+      <BaseCard className="py-4 pt-20 md:px-12 bg-white">
+        <BaseList className="max-w-xl">
+          {list.map((e) => (
+            <BaseListItem
+              key={e.id}
+              title={e.name}
+              end={
+                <BaseDropdown
+                  renderButton={
+                    <BaseButtonIcon size="sm" rounded="full" className="h-5 w-5">
+                      <EllipsisIcon></EllipsisIcon>
+                    </BaseButtonIcon>
+                  }
+                >
+                  <BaseDropdownItem>
+                    <TrashIcon></TrashIcon>
+                  </BaseDropdownItem>
+                </BaseDropdown>
+              }
+            >
+              <FileIcon className="h-6 w-6"></FileIcon>
+            </BaseListItem>
+          ))}
+        </BaseList>
+      </BaseCard>
       <UploadForm />
     </div>
   );
 }
-
-
