58c46c5e76b05417023b3deb8f58be2905eb7d1d
huayemao <huayemao4g@gmail.com>
2024-07-25 18:46:38 +0800

    fix: 修复分页查询条件

--- app/(home)/p/[page]/page.tsx
+++ app/(home)/p/[page]/page.tsx
@@ -1,9 +1,8 @@
 import { Posts } from "@/components/Posts";
 import { POSTS_COUNT_PER_PAGE } from "@/constants";
 import { PaginateOptions } from "@/lib/paginator";
-import { getPosts, getProcessedPosts } from "@/lib/posts";
-import prisma from "@/lib/prisma";
-import { Suspense, cache } from "react";
+import { getPageCount, getPosts, getProcessedPosts } from "@/lib/posts";
+import { Suspense } from "react";
 import Pagination from "../../Pagination";
 
 type SearchParams = PaginateOptions;
@@ -12,17 +11,15 @@ export const revalidate = 600;
 //https://beta.nextjs.org/docs/data-fetching/fetching#segment-cache-configuration
 
 export async function generateStaticParams() {
-  const count = await getPageCount(POSTS_COUNT_PER_PAGE);
+  const count = await getPageCount({
+    perPage: POSTS_COUNT_PER_PAGE,
+    protected: false,
+  });
   const params = Array.from({ length: count }, (_, i) => ({
     page: String(i + 1),
   }));
   return params.slice(0, 5);
 }
-
-const getPageCount = cache(async (perPage: number) => {
-  const itemCount = await prisma.posts.count();
-  return itemCount / (perPage || POSTS_COUNT_PER_PAGE);
-});
 
 export default async function Home({
   searchParams,
@@ -33,8 +30,13 @@ export default async function Home({
     page: string | number | undefined;
   };
 }) {
-  const posts = await getProcessedPosts(await getPosts({ page: params.page }));
-  const pageCount = await getPageCount(POSTS_COUNT_PER_PAGE);
+  const posts = await getProcessedPosts(
+    await getPosts({ page: params.page, protected: false })
+  );
+  const pageCount = await getPageCount({
+    perPage: POSTS_COUNT_PER_PAGE,
+    protected: false,
+  });
 
   const isFirstPage = !(params.page && Number(params.page) > 1);
 
