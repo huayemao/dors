622e14b37b9f29d87a8dfa6d5ee6a22dbe353c14
huayemao <huayemao4g@gmail.com>
2024-08-14 21:45:04 +0800

    fix: 修复 mdx 解析问题

--- lib/utils.ts
+++ lib/utils.ts
@@ -38,6 +38,7 @@ export async function getPexelImages(
 }
 
 export async function markdownToHtml(markdown) {
+  // @ts-ignore
   const result = await remark().use(html).process(markdown);
   return result.toString();
 }
