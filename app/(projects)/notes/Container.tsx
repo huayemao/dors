b210d7b2e0865bb6700b353fe108a539881e8b82
huayemao <huayemao4g@gmail.com>
2024-08-26 08:51:10 +0800

    feat: 修改小记页面 title

---
+++ app/(projects)/notes/Container.tsx
@@ -0,0 +1,82 @@
+"use client"
+import Prose from "@/components/Base/Prose";
+import { BaseTag } from "@shuriken-ui/react";
+import Route from "@/lib/client/createEntity/Route";
+import { Note } from "./constants";
+import { useEntity, useEntityDispatch } from "./contexts";
+import { NoteForm } from "./NoteForm";
+import NotesPage from "./page";
+
+export const Container = () => {
+    const state = useEntity();
+    const dispatch = useEntityDispatch();
+
+    return (
+        <>
+            <Route
+                renderEntityModalTitle={(e: Note) => (
+                    <>
+                        <span className="flex mb-2 gap-2 flex-nowrap  items-start overflow-x-auto py-1 leading-normal">
+                            {e.tags?.map((e) => (
+                                <div key={e} className="cursor-pointer flex-shrink-0">
+                                    <BaseTag
+                                        // onClick={() => {
+                                        //   dispatch({
+                                        //     type: "setTags",
+                                        //     payload: Array.from(new Set(filters.tags.concat(e))),
+                                        //   });
+                                        // }}
+                                        key={e}
+                                        size="sm"
+                                        variant="outline"
+                                        color="primary"
+                                    >
+                                        {e}
+                                    </BaseTag>
+                                </div>
+                            ))}
+                        </span>
+                        {
+                            <time className="text-xs text-slate-600">
+                                {new Date(e.id).toLocaleDateString()}
+                            </time>
+                        }
+                    </>
+                )}
+                renderEntity={(e: Note, { preview }) => (
+                    <div>
+                        {preview && (
+                            <div className="flex gap-2 flex-nowrap  items-start overflow-x-auto py-1">
+                                {e.tags?.map((e) => (
+                                    <div key={e} className="cursor-pointer flex-shrink-0">
+                                        <BaseTag
+                                            // onClick={() => {
+                                            //   dispatch({
+                                            //     type: "setTags",
+                                            //     payload: Array.from(new Set(filters.tags.concat(e))),
+                                            //   });
+                                            // }}
+                                            key={e}
+                                            size="sm"
+                                            variant="outline"
+                                            color="primary"
+                                        >
+                                            {e}
+                                        </BaseTag>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                        <Prose content={e.content}></Prose>
+                    </div>
+                )}
+                state={state}
+                dispatch={dispatch}
+                RootPage={NotesPage}
+                basename={"/notes"}
+                createForm={NoteForm}
+                updateForm={NoteForm}
+            ></Route>
+        </>
+    );
+};
--- app/(projects)/notes/layout.tsx
+++ app/(projects)/notes/layout.tsx
@@ -1,90 +1,18 @@
-"use client";
 import {
   EntityContextProvider,
-  useEntity,
-  useEntityDispatch,
 } from "./contexts";
-import { NoteForm } from "./NoteForm";
-import NotesPage from "./page";
-import Route from "@/lib/client/createEntity/Route";
 import { ClientOnly } from "@/components/ClientOnly";
-import Prose from "@/components/Base/Prose";
-import { Note } from "./constants";
-import { BaseTag } from "@shuriken-ui/react";
+import { Metadata } from "next";
+import { Container } from "./Container";
 
-const Container = () => {
-  const state = useEntity();
-  const dispatch = useEntityDispatch();
-
-  return (
-    <Route
-      renderEntityModalTitle={(e: Note) => (
-        <>
-          <span className="flex mb-2 gap-2 flex-nowrap  items-start overflow-x-auto py-1 leading-normal">
-            {e.tags?.map((e) => (
-              <div key={e} className="cursor-pointer flex-shrink-0">
-                <BaseTag
-                  // onClick={() => {
-                  //   dispatch({
-                  //     type: "setTags",
-                  //     payload: Array.from(new Set(filters.tags.concat(e))),
-                  //   });
-                  // }}
-                  key={e}
-                  size="sm"
-                  variant="outline"
-                  color="primary"
-                >
-                  {e}
-                </BaseTag>
-              </div>
-            ))}
-          </span>
-          {
-            <time className="text-xs text-slate-600">
-              {new Date(e.id).toLocaleDateString()}
-            </time>
-          }
-        </>
-      )}
-      renderEntity={(e: Note, { preview }) => (
-        <div>
-          {preview && (
-            <div className="flex gap-2 flex-nowrap  items-start overflow-x-auto py-1">
-              {e.tags?.map((e) => (
-                <div key={e} className="cursor-pointer flex-shrink-0">
-                  <BaseTag
-                    // onClick={() => {
-                    //   dispatch({
-                    //     type: "setTags",
-                    //     payload: Array.from(new Set(filters.tags.concat(e))),
-                    //   });
-                    // }}
-                    key={e}
-                    size="sm"
-                    variant="outline"
-                    color="primary"
-                  >
-                    {e}
-                  </BaseTag>
-                </div>
-              ))}
-            </div>
-          )}
-          <Prose content={e.content}></Prose>
-        </div>
-      )}
-      state={state}
-      dispatch={dispatch}
-      RootPage={NotesPage}
-      basename={"/notes"}
-      createForm={NoteForm}
-      updateForm={NoteForm}
-    ></Route>
-  );
+export const metadata: Metadata = {
+  title: 'UltraNotes',
+  description: 'local note app surpporting mdx with great style'
 };
 
-export default function QAsLayout({}) {
+
+
+export default function QAsLayout({ }) {
   return (
     <EntityContextProvider>
       <ClientOnly>
