20d2623c429dd5e3f5898e4c07920e6f385ea395
huayemao <huayemao4g@gmail.com>
2024-08-26 23:48:17 +0800

    chore: serve shiki files

--- components/Base/parsedMdx.tsx
+++ components/Base/parsedMdx.tsx
@@ -1,17 +1,17 @@
-"use client"
+"use client";
 import { parseMDXClient } from "@/lib/mdx/parseMDXClient";
 import { MDXContent } from "mdx/types";
 import { useEffect, useState } from "react";
 
 export default function ParsedMdx({ content }: { content: string }) {
-    const [result, setRes] = useState<MDXContent>();
-    useEffect(() => {
-        parseMDXClient(content).then(setRes);
-    }, [])
+  const [result, setRes] = useState<MDXContent>();
+  useEffect(() => {
+    parseMDXClient(content).then(setRes);
+  }, []);
 
-    return (
-        <>
-            {typeof result == 'function' ? result({}) : typeof result != 'string' ? result : content}
-        </>
-    );
+  return (
+    <>
+      {typeof result == "function" ? result({}) : !!result ? result : content}
+    </>
+  );
 }
--- lib/mdx/parseMDXClient.tsx
+++ lib/mdx/parseMDXClient.tsx
@@ -8,14 +8,179 @@ import remarkMath from "remark-math";
 import * as runtime from "react/jsx-runtime";
 import { components } from "@/lib/mdx/useComponents";
 import { myRemarkPlugin } from "./myRemarkPlugin";
-import { BUNDLED_LANGUAGES, setCDN, setWasm } from 'shiki'
+import { setWasm } from "shiki";
+import nord from "shiki/themes/nord.json";
 
+// declare type Lang =
+//   | "abap"
+//   | "actionscript-3"
+//   | "ada"
+//   | "apache"
+//   | "apex"
+//   | "apl"
+//   | "applescript"
+//   | "asm"
+//   | "astro"
+//   | "awk"
+//   | "ballerina"
+//   | "bat"
+//   | "batch"
+//   | "berry"
+//   | "be"
+//   | "bibtex"
+//   | "bicep"
+//   | "c"
+//   | "clojure"
+//   | "clj"
+//   | "cobol"
+//   | "codeql"
+//   | "ql"
+//   | "coffee"
+//   | "cpp"
+//   | "crystal"
+//   | "csharp"
+//   | "c#"
+//   | "css"
+//   | "cue"
+//   | "d"
+//   | "dart"
+//   | "diff"
+//   | "docker"
+//   | "dream-maker"
+//   | "elixir"
+//   | "elm"
+//   | "erb"
+//   | "erlang"
+//   | "fish"
+//   | "fsharp"
+//   | "f#"
+//   | "gherkin"
+//   | "git-commit"
+//   | "git-rebase"
+//   | "gnuplot"
+//   | "go"
+//   | "graphql"
+//   | "groovy"
+//   | "hack"
+//   | "haml"
+//   | "handlebars"
+//   | "hbs"
+//   | "haskell"
+//   | "hcl"
+//   | "hlsl"
+//   | "html"
+//   | "ini"
+//   | "java"
+//   | "javascript"
+//   | "js"
+//   | "jinja-html"
+//   | "json"
+//   | "jsonc"
+//   | "jsonnet"
+//   | "jssm"
+//   | "fsl"
+//   | "jsx"
+//   | "julia"
+//   | "jupyter"
+//   | "kotlin"
+//   | "latex"
+//   | "less"
+//   | "lisp"
+//   | "logo"
+//   | "lua"
+//   | "make"
+//   | "makefile"
+//   | "markdown"
+//   | "md"
+//   | "marko"
+//   | "matlab"
+//   | "mdx"
+//   | "nginx"
+//   | "nim"
+//   | "nix"
+//   | "objective-c"
+//   | "objc"
+//   | "objective-cpp"
+//   | "ocaml"
+//   | "pascal"
+//   | "perl"
+//   | "php"
+//   | "plsql"
+//   | "postcss"
+//   | "powershell"
+//   | "ps"
+//   | "ps1"
+//   | "prisma"
+//   | "prolog"
+//   | "pug"
+//   | "jade"
+//   | "puppet"
+//   | "purescript"
+//   | "python"
+//   | "py"
+//   | "r"
+//   | "raku"
+//   | "perl6"
+//   | "razor"
+//   | "rel"
+//   | "riscv"
+//   | "ruby"
+//   | "rb"
+//   | "rust"
+//   | "rs"
+//   | "sas"
+//   | "sass"
+//   | "scala"
+//   | "scheme"
+//   | "scss"
+//   | "shaderlab"
+//   | "shader"
+//   | "shellscript"
+//   | "shell"
+//   | "bash"
+//   | "sh"
+//   | "zsh"
+//   | "smalltalk"
+//   | "solidity"
+//   | "sparql"
+//   | "sql"
+//   | "ssh-config"
+//   | "stata"
+//   | "stylus"
+//   | "styl"
+//   | "svelte"
+//   | "swift"
+//   | "system-verilog"
+//   | "tasl"
+//   | "tcl"
+//   | "tex"
+//   | "toml"
+//   | "tsx"
+//   | "turtle"
+//   | "twig"
+//   | "typescript"
+//   | "ts"
+//   | "vb"
+//   | "cmd"
+//   | "verilog"
+//   | "vhdl"
+//   | "viml"
+//   | "vim"
+//   | "vimscript"
+//   | "vue-html"
+//   | "vue"
+//   | "wasm"
+//   | "wenyan"
+//   | "文言"
+//   | "xml"
+//   | "xsl"
+//   | "yaml"
+//   | "zenscript";
 
 export async function parseMDXClient(mdx: string) {
-  setCDN('https://unpkg.com/shiki@0.10.1/');
-  const responseWasm = await fetch("https://unpkg.com/shiki/dist/onig.wasm");
+  const responseWasm = await fetch("/shiki/onig.wasm");
   const wasmArrayBuffer = await responseWasm.arrayBuffer();
-  setWasm(wasmArrayBuffer);
+  await setWasm(wasmArrayBuffer);
 
   // @ts-ignore
   const res = await evaluate(mdx, {
@@ -26,24 +191,41 @@ export async function parseMDXClient(mdx: string) {
     remarkRehypeOptions: {
       allowDangerousHtml: true,
     },
-    rehypePlugins: [
-      [rehypeRaw, { passThrough: nodeTypes }],
-      [rehypeKatex],
-    ],
+    rehypePlugins: [[rehypeRaw, { passThrough: nodeTypes }], [rehypeKatex]],
     remarkPlugins: [
       [
-        remarkShikiTwoslash
-        , { theme: 'nord', languages: ['typescript'] }
+        remarkShikiTwoslash,
+        {
+          theme: nord,
+          langs: [
+            "ts",
+            "js",
+            "jsx",
+            "tsx",
+            "json",
+            "html",
+            "css",
+            "powershell",
+            "bash",
+            "java",
+            "c",
+            "cpp",
+            "sql",
+            "vue",
+          ],
+          paths: {
+            languages: "/shiki/languages/",
+          },
+        },
       ],
       remarkGfm,
       remarkMath,
       remarkDirective,
       myRemarkPlugin,
     ],
-    format: 'mdx',
-
+    format: "mdx",
   });
-  console.log(res.default)
+  console.log(res.default);
 
-  return res.default
+  return res.default;
 }
