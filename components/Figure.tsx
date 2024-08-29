c5c612d062a97cbd2a6fea82da17e9db5e444083
huayemao <huayemao4g@gmail.com>
2024-08-29 15:52:37 +0800

    feat: 图片标签支持视频和音乐

--- components/Figure.tsx
+++ components/Figure.tsx
@@ -1,20 +1,39 @@
 "use client";
+import mime from "mime";
 import { useEffect, useRef } from "react";
 
 export function Figure(props) {
+  const { src } = props;
+  const mimetype = mime.getType(src)
+  mime.getType(src)
   const ref = useRef<HTMLAnchorElement>(null);
   useEffect(() => {
     if (!ref.current) {
       return;
     }
     const current = ref.current;
-    const img = current.querySelector("img")!;
+    const img = current.querySelector("img");
+    if (!img) {
+      return
+    }
     img.onload = (e) => {
       current.href = img.src || "";
       current.dataset["pswpWidth"] = "" + img.naturalWidth;
       current.dataset["pswpHeight"] = "" + img.naturalHeight;
     };
   }, []);
+  if (mimetype?.startsWith('video')) {
+    return <video controls preload="metadata">
+      <source src={src} type={mimetype} />
+      Your browser does not support the video tag.
+    </video>
+  }
+  else if (mimetype?.startsWith('audio')) {
+    return <audio controls preload="metadata">
+      <source src={src} type={mimetype} />
+      Your browser does not support the aduio tag.
+    </audio>
+  }
   return (
     <a
       suppressHydrationWarning
