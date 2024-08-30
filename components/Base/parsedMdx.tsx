502c058d47c5b1c6c451185a39a3005114c96517
huayemao <huayemao4g@gmail.com>
2024-08-30 13:58:31 +0800

    feat: 富文本内容添加加载动画

--- components/Base/parsedMdx.tsx
+++ components/Base/parsedMdx.tsx
@@ -1,17 +1,28 @@
 "use client";
 import { parseMDXClient } from "@/lib/mdx/parseMDXClient";
+import { BasePlaceload } from "@shuriken-ui/react";
 import { MDXContent } from "mdx/types";
 import { useEffect, useState } from "react";
 
 export default function ParsedMdx({ content }: { content: string }) {
   const [result, setRes] = useState<MDXContent>();
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    parseMDXClient(content).then(setRes);
+    parseMDXClient(content).then(setRes).finally(() => {
+      setLoading(false)
+    });
+
   }, [content]);
 
   return (
     <>
-      {typeof result == "function" ? result({}) : !!result ? result : content}
+      {typeof result == "function" ? result({}) : !loading ? result :
+        <div className="space-y-2">
+          <BasePlaceload className="h-4 w-full rounded" />
+          <BasePlaceload className="h-4 w-3/4 rounded" />
+          <BasePlaceload className="h-4 w-full rounded" />
+        </div>
+      }
     </>
   );
 }
