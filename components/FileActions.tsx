0028a1d60c638e81cd50b204343f8661f94f64cb
huayemao <huayemao4g@gmail.com>
2024-09-02 11:26:36 +0800

    fix: 修复文件管理页错误

--- components/FileActions.tsx
+++ components/FileActions.tsx
@@ -1,12 +1,10 @@
+"use client"
 import { copyTextToClipboard, humanFileSize } from "@/lib/utils";
 import { BaseDropdown, BaseDropdownItem } from "@shuriken-ui/react";
 import { CopyIcon, FileIcon, ImageIcon, TrashIcon } from "lucide-react";
-import { SITE_META } from "@/constants";
-import LightBox from "@/components/Base/LightBox";
-import { Figure } from "@/components/Figure";
-import { Popover } from "@headlessui/react";
 import { getFilePath } from "@/lib/client/utils/getFilePath";
 import { FileItem } from "./FileList";
+import toast from "react-hot-toast";
 export function FileActions({ e, admin }: { e: FileItem; admin: boolean }) {
   return (
     <BaseDropdown variant="context">
@@ -14,7 +12,9 @@ export function FileActions({ e, admin }: { e: FileItem; admin: boolean }) {
         start={<CopyIcon className="w-5 h-5"></CopyIcon>}
         title="复制文件路径"
         onClick={() => {
-          copyTextToClipboard(getFilePath(e.name));
+          copyTextToClipboard(getFilePath(e.name)).then(() => {
+            toast("已复制到剪贴板")
+          });
         }}
       ></BaseDropdownItem>
       {admin && (
@@ -24,8 +24,10 @@ export function FileActions({ e, admin }: { e: FileItem; admin: boolean }) {
           onClick={() => {
             fetch("/api/files/" + e.id, { method: "DELETE" }).then((res) => {
               if (res.status == 200) {
-                alert("删除成功");
+                toast("删除成功");
               }
+            }).catch((e) => {
+              toast("操作失败")
             });
           }}
         ></BaseDropdownItem>
