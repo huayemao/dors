6debfedb63a09c23c5359e3b97f14c1715e96be6
huayemao <huayemao4g@gmail.com>
2024-09-24 11:52:13 +0800

    fix: 修复内容页资源链接部分缺失的问题

--- app/(content)/layout.tsx
+++ app/(content)/layout.tsx
@@ -1,4 +1,6 @@
 import { Nav } from "@/components/Nav";
+import { CategoriesContextProvider } from "@/contexts/categories";
+import { getAllCategories } from "@/lib/server/categories";
 import { getResourceItems } from "@/lib/server/resource";
 import { unstable_cache } from "next/cache";
 
@@ -10,11 +12,13 @@ export default async function ContentLayout({
   params: any;
 }) {
   const resourceItems = await getResourceItems();
-
+  const categories = await getAllCategories({ includeHidden: true });
   return (
-    <div className="z-0">
-      <Nav resourceItems={resourceItems}></Nav>
-      <div className="pt-20 bg-muted-100 dark:bg-muted-900 min-h-screen">{children}</div>
-    </div>
+    <CategoriesContextProvider Categories={categories}>
+      <div className="z-0">
+        <Nav resourceItems={resourceItems}></Nav>
+        <div className="pt-20 bg-muted-100 dark:bg-muted-900 min-h-screen">{children}</div>
+      </div>
+    </CategoriesContextProvider>
   );
 }
