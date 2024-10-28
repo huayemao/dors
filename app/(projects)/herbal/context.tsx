a0f9890e67e96305f840d985a851d9bdebec77ed
huayemao <huayemao4g@gmail.com>
2024-10-28 09:32:21 +0800

    fix: 修复中药材格式处理工具 setList 后不更新界面的问题

--- app/(projects)/herbal/context.tsx
+++ app/(projects)/herbal/context.tsx
@@ -1,40 +1,45 @@
 "use client";
 
 import { getTags } from "@/lib/server/tags";
-import { Dispatch, FC, PropsWithChildren, createContext, useContext, useReducer, useState } from "react";
+import {
+  Dispatch,
+  FC,
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useReducer,
+  useState,
+} from "react";
 
 const initialState = {
-    list: [] as any[]
+  list: [] as any[],
 };
 
-export const HerbalContext =
-    createContext(initialState);
+export const HerbalContext = createContext(initialState);
 
-export const HerbalContextProvider: FC<
-    PropsWithChildren<{
-    }>
-> = ({ children }) => {
-    const [state, dispatch] = useReducer((_state, { payload }) => {
-        return Object.assign(_state, payload)
-    },
-        initialState)
-    return (
-        <HerbalContext.Provider value={state}>
-            <HerbalDispatchContext.Provider value={dispatch}>
-                {children}
-            </HerbalDispatchContext.Provider>
-        </HerbalContext.Provider>
-    );
+export const HerbalContextProvider: FC<PropsWithChildren<{}>> = ({
+  children,
+}) => {
+  const [state, dispatch] = useReducer((_state, { payload }) => {
+    return Object.assign({ ..._state }, payload);
+  }, initialState);
+  return (
+    <HerbalContext.Provider value={state}>
+      <HerbalDispatchContext.Provider value={dispatch}>
+        {children}
+      </HerbalDispatchContext.Provider>
+    </HerbalContext.Provider>
+  );
 };
 
-const HerbalDispatchContext = createContext<
-    Dispatch<{ payload: any }>
->(() => { });
+const HerbalDispatchContext = createContext<Dispatch<{ payload: any }>>(
+  () => {}
+);
 
 export function useHerbalDispatch() {
-    return useContext(HerbalDispatchContext);
+  return useContext(HerbalDispatchContext);
 }
 
 export function useHerbalContext() {
-    return useContext(HerbalContext);
+  return useContext(HerbalContext);
 }
