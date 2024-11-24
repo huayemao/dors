c6d319ec6e4e2d21a2d0573153832758735ef1a2
huayemao <huayemao4g@gmail.com>
2024-11-24 21:18:41 +0800

    fix: 为 ClientOnly 组件添加 ErrorBoundary

--- lib/client/utils/withClientOnly.tsx
+++ lib/client/utils/withClientOnly.tsx
@@ -1,5 +1,8 @@
 import { ClientOnly } from "@/components/ClientOnly";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 import React, { useEffect, useState } from "react";
+
+const ErrorComp = ({ error }) => <div>出错了：{error.message}</div>;
 
 function withClientOnly<T extends React.ComponentType<any>>(
   WrappedComponent: T
@@ -8,7 +11,9 @@ function withClientOnly<T extends React.ComponentType<any>>(
   const WrapperComponent = (props: React.ComponentProps<T>) => {
     return (
       <ClientOnly>
-        <WrappedComponent {...props} />
+        <ErrorBoundary errorComponent={ErrorComp}>
+          <WrappedComponent {...props} />
+        </ErrorBoundary>
       </ClientOnly>
     );
   };
