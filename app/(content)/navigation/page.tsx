95107c1fbda583de04478f7508327f4d05a6e847
huayemao <huayemao4g@gmail.com>
2024-11-24 22:05:28 +0800

    fix: 优化导航页样式

--- app/(content)/navigation/page.tsx
+++ app/(content)/navigation/page.tsx
@@ -5,6 +5,7 @@ import { unstable_cache } from "next/cache";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import Revalidate from "./Revalidate";
+import { cn } from "@/lib/utils";
 
 const getNavContent = unstable_cache(
   async function () {
@@ -40,20 +41,23 @@ export default async function Navigation() {
 
   return (
     <>
-      <header className="sticky top-0 flex items-center justify-center backdrop-blur bg-transparent h-12">
-        <BaseHeading as="h1" className="text-center ">
+      <main className="w-full min-h-full dark:bg-muted-900 max-w-6xl mx-auto flex-1 p-6  space-y-4">
+        <BaseHeading size="3xl" as="h2">
           花野猫的导航页
         </BaseHeading>
-        <div className="absolute right-4">
+        <div className="text-right">
           <Revalidate></Revalidate>
         </div>
-      </header>
-      <main className="bg-muted-50 w-full dark:bg-muted-900 max-w-full flex-1 p-8">
-        <div className=" masonry sm:masonry-sm md:masonry-md mb-auto">
-          {cats.map((e) => {
+        <div className="masonry sm:masonry-sm md:masonry-md mb-auto">
+          {cats.map((e, i) => {
             return (
-              <BaseCard key={e.id} className="p-4 break-inside-avoid">
-                <BaseHeading as="h2">{e.tags}</BaseHeading>
+              <BaseCard
+                key={e.id}
+                className={cn("my-4 p-4 break-inside-avoid", {
+                  "mt-0": i === 0,
+                })}
+              >
+                <BaseHeading as="h3">{e.tags}</BaseHeading>
                 <Prose content={e.content}></Prose>
               </BaseCard>
             );
