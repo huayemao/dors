cb8ca4babb97e55cebd497c5e13dcf5b39f87aab
huayemao <huayemao4g@gmail.com>
2024-11-27 22:30:13 +0800

    fix: 缩小主页缓存失效条件

--- lib/client/utils/retalidate.ts
+++ lib/client/utils/retalidate.ts
@@ -1,11 +1,23 @@
 import { POSTS_COUNT_PER_PAGE } from "@/constants";
 import { revalidatePath, revalidateTag } from "next/cache";
 import prisma from "../../prisma";
+import { getHiddenCategoryIds } from "@/lib/server/categories";
 
 export async function revalidateHomePage(id: number) {
+  const ids = await getHiddenCategoryIds();
   const firstPagePosts = await prisma.posts.findMany({
     select: {
       id: true,
+    },
+    where: {
+      protected: false,
+      posts_category_links: {
+        none: {
+          category_id: {
+            in: ids
+          }
+        }
+      }
     },
     take: POSTS_COUNT_PER_PAGE,
     orderBy: {
