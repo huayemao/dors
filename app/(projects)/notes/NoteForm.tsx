2191a4762bd3a6915b387e0351e18e6c2065a3d9
huayemao <huayemao4g@gmail.com>
2025-03-08 14:09:36 +0800

    feat: 小记编辑页新增移动端预览功能

--- app/(projects)/notes/NoteForm.tsx
+++ app/(projects)/notes/NoteForm.tsx
@@ -5,6 +5,7 @@ import {
   BaseCard,
   BaseInput,
   BaseSelect,
+  BaseTabSlider,
 } from "@shuriken-ui/react";
 import { DOMAttributes, FC, PropsWithChildren, useMemo, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
@@ -140,34 +141,39 @@ export const NoteForm: FC<PropsWithChildren> = ({ children }) => {
                 </div>
               </div>
             </fieldset>
-            <div className="col-span-12">
-              <div className="relative">
-                <label
-                  htmlFor="content"
-                  className="nui-label pb-1 text-[0.825rem]"
-                >
-                  内容
-                </label>
-                <div className="lg:grid grid-cols-2 gap-6">
-                  <TextareaAutosize
-                    key={currentEntity?.id}
-                    id="content"
-                    name="content"
-                    className="nui-focus border-muted-300 placeholder:text-muted-300 focus:border-muted-300 focus:shadow-muted-300/50 dark:border-muted-700 dark:bg-muted-900/75 dark:text-muted-200 dark:placeholder:text-muted-500 dark:focus:border-muted-700 dark:focus:shadow-muted-800/50 peer w-full border bg-white font-sans transition-all duration-300 focus:shadow-lg disabled:cursor-not-allowed disabled:opacity-75 min-h-48 text-sm leading-[1.6] rounded resize-none p-2"
-                    placeholder="请输入内容"
-                    minRows={10}
-                    defaultValue={currentEntity?.content || ""}
-                    onChange={(e) => {
-                      setContent(e.target.value);
-                    }}
-                  />
-                  {/* <div className="bg-muted-50 p-4"> */}
-                  <BaseCard shadow="flat" className="px-4 bg-white">
-                    <Prose content={content}></Prose>
-                  </BaseCard>
-                  {/* </div> */}
-                </div>
-              </div>
+            <div className="col-span-12 mt-4 min-h-72">
+              <BaseTabSlider
+                size="sm"
+                tabs={[
+                  { label: "编辑", value: "edit" },
+                  { label: "预览", value: "preview" },
+                ]}
+                defaultValue="edit"
+              >
+                {(activeValue) => (
+                  <>
+                    {activeValue === "edit" && (
+                      <TextareaAutosize
+                        key={currentEntity?.id}
+                        id="content"
+                        name="content"
+                        className="nui-focus border-muted-300 placeholder:text-muted-300 focus:border-muted-300 focus:shadow-muted-300/50 dark:border-muted-700 dark:bg-muted-900/75 dark:text-muted-200 dark:placeholder:text-muted-500 dark:focus:border-muted-700 dark:focus:shadow-muted-800/50 peer w-full border bg-white font-sans transition-all duration-300 focus:shadow-lg disabled:cursor-not-allowed disabled:opacity-75 min-h-48 text-sm leading-[1.6] rounded resize-none p-2"
+                        placeholder="请输入内容"
+                        minRows={10}
+                        defaultValue={currentEntity?.content || ""}
+                        onChange={(e) => {
+                          setContent(e.target.value);
+                        }}
+                      />
+                    )}
+                    {activeValue === "preview" && (
+                      <BaseCard shadow="flat" className="px-4 bg-white">
+                        <Prose content={content}></Prose>
+                      </BaseCard>
+                    )}
+                  </>
+                )}
+              </BaseTabSlider>
             </div>
           </div>
         </div>
