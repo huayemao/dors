5c16fca58aec98642876aec1042e093f2cbb092d
huayemao <huayemao4g@gmail.com>
2025-03-08 15:41:39 +0800

    fix: 修复小记编辑页预览后编辑内容消失的问题

--- app/(projects)/notes/NoteForm.tsx
+++ app/(projects)/notes/NoteForm.tsx
@@ -16,6 +16,7 @@ import { BaseAutocomplete } from "@/components/Base/Autocomplete";
 import { useNavigate } from "react-router-dom";
 import Prose from "@/components/Base/Prose";
 import { HIDDEN_TAGS } from "@/components/Notes/constants";
+import { useMediaQuery } from "@uidotdev/usehooks";
 
 export const NoteForm: FC<PropsWithChildren> = ({ children }) => {
   const close = useCloseModal();
@@ -84,7 +85,7 @@ export const NoteForm: FC<PropsWithChildren> = ({ children }) => {
       ),
     [entityList]
   );
-
+  const isDeskTop = useMediaQuery("only screen and (min-width : 960px)");
   return (
     <form method="POST" onSubmit={handleSubmit}>
       <div className="ltablet:col-span-6 col-span-12 md:col-span-12">
@@ -142,38 +143,61 @@ export const NoteForm: FC<PropsWithChildren> = ({ children }) => {
               </div>
             </fieldset>
             <div className="col-span-12 mt-4 min-h-72">
-              <BaseTabSlider
-                size="sm"
-                tabs={[
-                  { label: "编辑", value: "edit" },
-                  { label: "预览", value: "preview" },
-                ]}
-                defaultValue="edit"
-              >
-                {(activeValue) => (
-                  <>
-                    {activeValue === "edit" && (
-                      <TextareaAutosize
-                        key={currentEntity?.id}
-                        id="content"
-                        name="content"
-                        className="nui-focus border-muted-300 placeholder:text-muted-300 focus:border-muted-300 focus:shadow-muted-300/50 dark:border-muted-700 dark:bg-muted-900/75 dark:text-muted-200 dark:placeholder:text-muted-500 dark:focus:border-muted-700 dark:focus:shadow-muted-800/50 peer w-full border bg-white font-sans transition-all duration-300 focus:shadow-lg disabled:cursor-not-allowed disabled:opacity-75 min-h-48 text-sm leading-[1.6] rounded resize-none p-2"
-                        placeholder="请输入内容"
-                        minRows={10}
-                        defaultValue={currentEntity?.content || ""}
-                        onChange={(e) => {
-                          setContent(e.target.value);
-                        }}
-                      />
-                    )}
-                    {activeValue === "preview" && (
-                      <BaseCard shadow="flat" className="px-4 bg-white">
-                        <Prose content={content}></Prose>
-                      </BaseCard>
-                    )}
-                  </>
-                )}
-              </BaseTabSlider>
+              {isDeskTop && (
+                <div className="lg:grid grid-cols-2 gap-6">
+                  <TextareaAutosize
+                    key={currentEntity?.id}
+                    id="content"
+                    name="content"
+                    className="nui-focus border-muted-300 placeholder:text-muted-300 focus:border-muted-300 focus:shadow-muted-300/50 dark:border-muted-700 dark:bg-muted-900/75 dark:text-muted-200 dark:placeholder:text-muted-500 dark:focus:border-muted-700 dark:focus:shadow-muted-800/50 peer w-full border bg-white font-sans transition-all duration-300 focus:shadow-lg disabled:cursor-not-allowed disabled:opacity-75 min-h-48 text-sm leading-[1.6] rounded resize-none p-2"
+                    placeholder="请输入内容"
+                    minRows={10}
+                    defaultValue={currentEntity?.content || ""}
+                    onChange={(e) => {
+                      setContent(e.target.value);
+                    }}
+                  />
+                  {/* <div className="bg-muted-50 p-4"> */}
+                  <BaseCard shadow="flat" className="px-4 bg-white">
+                    <Prose content={content}></Prose>
+                  </BaseCard>
+                  {/* </div> */}
+                </div>
+              )}
+              {!isDeskTop && (
+                <BaseTabSlider
+                  size="sm"
+                  tabs={[
+                    { label: "编辑", value: "edit" },
+                    { label: "预览", value: "preview" },
+                  ]}
+                  defaultValue="edit"
+                >
+                  {(activeValue) => (
+                    <>
+                      {activeValue === "edit" && (
+                        <TextareaAutosize
+                          key={currentEntity?.id}
+                          id="content"
+                          name="content"
+                          className="nui-focus border-muted-300 placeholder:text-muted-300 focus:border-muted-300 focus:shadow-muted-300/50 dark:border-muted-700 dark:bg-muted-900/75 dark:text-muted-200 dark:placeholder:text-muted-500 dark:focus:border-muted-700 dark:focus:shadow-muted-800/50 peer w-full border bg-white font-sans transition-all duration-300 focus:shadow-lg disabled:cursor-not-allowed disabled:opacity-75 min-h-48 text-sm leading-[1.6] rounded resize-none p-2"
+                          placeholder="请输入内容"
+                          minRows={10}
+                          defaultValue={content}
+                          onChange={(e) => {
+                            setContent(e.target.value);
+                          }}
+                        />
+                      )}
+                      {activeValue === "preview" && (
+                        <BaseCard shadow="flat" className="px-4 bg-white">
+                          <Prose content={content}></Prose>
+                        </BaseCard>
+                      )}
+                    </>
+                  )}
+                </BaseTabSlider>
+              )}
             </div>
           </div>
         </div>
