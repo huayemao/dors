91fe46ef9452dcd4809e9649a6a724da72d7fc36
huayemao <huayemao4g@gmail.com>
2025-05-09 16:48:46 +0800

    feat: 新增清楚文章缓存按钮

--- app/admin/settings/RevalidateButton.tsx
+++ app/admin/settings/RevalidateButton.tsx
@@ -4,13 +4,22 @@ import { BaseButton } from "@shuriken-ui/react";
 
 export function RevalidateButton() {
   return (
-    <BaseButton
-      onClick={() => {
-        fetch("/api/revalidate?path=/");
-        fetch("/api/revalidate?path=/(home)");
-      }}
-    >
-      重新渲染首页
-    </BaseButton>
+    <div className="flex flex-col gap-4">
+      <BaseButton
+        onClick={() => {
+          fetch("/api/revalidate?path=/");
+          fetch("/api/revalidate?path=/(home)");
+        }}
+      >
+        重新渲染首页
+      </BaseButton>
+      <BaseButton
+        onClick={() => {
+          fetch("/api/revalidate?tag=posts");
+        }}
+      >
+        清楚文章缓存
+      </BaseButton>
+    </div>
   );
 }
