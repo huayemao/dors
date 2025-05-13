d6f5ede295fe493f9234e8d9b5aa1859ad8fc409
huayemao <huayemao4g@gmail.com>
2025-05-13 10:14:04 +0800

    feat: 文章管理页面新增分页功能

--- app/admin/posts/page.tsx
+++ app/admin/posts/page.tsx
@@ -1,18 +1,39 @@
 import { Panel } from "@/components/Base/Panel";
-import { getPosts } from "@/lib/server/posts";
+import { getPosts, getPageCount } from "@/lib/server/posts";
 import { BaseButtonAction } from "@shuriken-ui/react";
 import { PostsTable } from "./PostsTable";
+import { PaginateOptions } from "@/lib/paginator";
+import Pagination from "@/components/Pagination";
+import { Suspense } from "react";
 
-export default async function PostsPage() {
-  const posts = await getPosts();
+type SearchParams = PaginateOptions;
+
+export default async function PostsPage({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) {
+  const posts = await getPosts({
+    ...searchParams,
+    includeHiddenCategories: true,
+  });
+  const pageCount = await getPageCount({ includeHiddenCategories: true });
 
   return (
-    <>
-      <BaseButtonAction href={"/admin/posts/create"}>创建文章</BaseButtonAction>
+    <div className="space-y-4">
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold text-muted-700 dark:text-white">文章管理</h1>
+        <BaseButtonAction href={"/admin/posts/create"}>创建文章</BaseButtonAction>
+      </div>
       <Panel title="文章列表" className="max-w-full">
         <PostsTable posts={posts}></PostsTable>
+        <div className="mt-4">
+          <Suspense>
+            <Pagination pageCount={pageCount}></Pagination>
+          </Suspense>
+        </div>
       </Panel>
-    </>
+    </div>
   );
 }
 
