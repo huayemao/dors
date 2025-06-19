1da7b3fd3871394c77046219ca64b302d8b18f6d
huayemao <huayemao4g@gmail.com>
2025-06-19 21:47:54 +0800

    fix: 修复 sw 缓存错误响应的问题

--- public/sw.js
+++ public/sw.js
@@ -17,32 +17,48 @@ const ASSETS = [
   // "/shiki/languages/yaml.tmLanguage.json",
   // "/shiki/onig.wasm",
 ];
-importScripts('/version.js')
-const DYNAMIC_PATHS = ['/_next/static/chunks', 'favicon.ico', "/herbal", "/navigation", "/notes", "/apps"]
-const SSR_PATHS = ['/navigation?_rsc', "/notes?_rsc", "/apps"]
+importScripts("/version.js");
+const DYNAMIC_PATHS = [
+  "/_next/static/chunks",
+  "favicon.ico",
+  "/herbal",
+  "/navigation",
+  "/notes",
+  "/apps",
+];
+const SSR_PATHS = ["/navigation?_rsc", "/notes?_rsc", "/apps?_rsc"];
 const STABLE_DYNAMIC_PATHS = [
   "/api/files",
-  '/shiki/',
-  '/_next/image',
-  'https://unpkg.com/@ffmpeg/',
-  'twimg.com',
-  'sns-bak-v1.xhscdn.com'
-]
-
+  "/shiki/",
+  "/_next/image",
+  "https://unpkg.com/@ffmpeg/",
+  "https://i0.wp.com/",
+  "https://i1.wp.com/",
+  "https://i2.wp.com/",
+  "https://i3.wp.com/",
+  "twimg.com",
+  "sns-bak-v1.xhscdn.com",
+];
 
 self.addEventListener("fetch", (e) => {
   const { request: s } = e,
     url = new URL(s.url);
-  const isDynamicCache = DYNAMIC_PATHS.some(s => url.href.includes(s))
-  const isStableDynamicCache = STABLE_DYNAMIC_PATHS.some(s => url.href.includes(s))
-  const isAssetsCache = ASSETS.some(s => url.href.includes(s))
-  
+  const isSameOrigin = self.location.origin === url.origin;
+  const isDynamicCache = DYNAMIC_PATHS.some(
+    (s) => isSameOrigin && url.href.includes(s)
+  );
+  const isStableDynamicCache = STABLE_DYNAMIC_PATHS.some((s) =>
+    s.startsWith("/")
+      ? isSameOrigin && url.href.includes(s)
+      : url.href.includes(s)
+  );
+  const isAssetsCache = ASSETS.some(
+    (s) => isSameOrigin && url.href.includes(s)
+  );
+
   if (
-    ("GET" === s.method &&
-      (self.location.origin === url.origin || url.href.includes('unpkg.com')) && (
-        isAssetsCache || isDynamicCache || isStableDynamicCache
-      )
-    )
+    "GET" === s.method &&
+    (isAssetsCache || isDynamicCache || isStableDynamicCache)
   ) {
     e.respondWith(
       (async () => {
@@ -54,19 +70,22 @@ self.addEventListener("fetch", (e) => {
 
         // 如果缓存中没有，则获取新响应
         const response = await fetch(s);
-        
-        // 根据不同的缓存策略存储响应
-        if (isStableDynamicCache) {
-          const cache = await caches.open("STABLE_CACHE");
-          await cache.put(s, response.clone());
-        } else if (isDynamicCache) {
-          const isSSRPath = SSR_PATHS.find(path => url.href.includes(path));
-          if (!isSSRPath) {
-            const cache = await caches.open(VERSION);
+
+        if (response.status.toString().startsWith("2")) {
+          // 如果响应状态是2xx，才需要缓存
+          // 根据不同的缓存策略存储响应
+          if (isStableDynamicCache) {
+            const cache = await caches.open("STABLE_CACHE");
             await cache.put(s, response.clone());
+          } else if (isDynamicCache) {
+            const isSSRPath = SSR_PATHS.find((path) => url.href.includes(path));
+            if (!isSSRPath) {
+              const cache = await caches.open(VERSION);
+              await cache.put(s, response.clone());
+            }
           }
         }
-        
+
         return response;
       })()
     );
@@ -94,23 +113,28 @@ self.addEventListener("fetch", (e) => {
     }
 
     if (type === "revalidate-page") {
-      caches.open(VERSION).then((cache) => {
-        cache.keys().then((requests) => {
-          const matched = requests.find(request => request.url.includes(path))
-          if (matched) {
-            cache.delete(matched)
-          }
+      caches
+        .open(VERSION)
+        .then((cache) => {
+          cache.keys().then((requests) => {
+            const matched = requests.find((request) =>
+              request.url.includes(path)
+            );
+            if (matched) {
+              cache.delete(matched);
+            }
+          });
         })
-      }).then(() => {
-        console.log(`已清除路径 ${path} 的缓存`);
-        self.clients.matchAll().then(clients => {
-          clients.forEach(client => {
-            client.postMessage({
-              type: "revalidate-success",
-              path
+        .then(() => {
+          console.log(`已清除路径 ${path} 的缓存`);
+          self.clients.matchAll().then((clients) => {
+            clients.forEach((client) => {
+              client.postMessage({
+                type: "revalidate-success",
+                path,
+              });
             });
           });
         });
-      });
     }
   });
