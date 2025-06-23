4e59b01575a1312feb611a21cedbf0bc50a058c8
huayemao <huayemao4g@gmail.com>
2025-06-23 16:08:02 +0800

    feat: 优化 previewModal

--- components/Base/PreviewModal.tsx
+++ components/Base/PreviewModal.tsx
@@ -23,37 +23,33 @@ export const PreviewModal = ({
         exit={{ opacity: 0 }}
         transition={{ duration: 0.3 }}
         className="fixed inset-0 bg-black bg-opacity-25"
-      />
-
-      <div className="fixed inset-0 overflow-y-auto">
-        <div className="flex min-h-full items-center justify-center p-4 text-center">
-          <motion.div
-            initial={{ opacity: 0, scale: 0.95 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0.95 }}
-            transition={{ duration: 0.3 }}
-            className="flex-1 max-w-full"
-          >
-            <Dialog.Panel className="min-h-[80vh] w-full transform overflow-hidden flex justify-center items-center bg-white dark:bg-muted-800 p-6 lg:p-8 text-left align-middle shadow-xl relative">
-              <div className="mt-2 flex justify-center min-w-[95%] md:min-w-[75%] ">
-                {loading ? (
-                  <div className="flex justify-center items-center w-full h-32">
-                    加载中...
-                  </div>
-                ) : (
-                  children
-                )}
-              </div>
-              <div className="absolute right-4 top-4">
-                <BaseButtonClose
-                  type="button"
-                  onClick={onClose}
-                />
-              </div>
-            </Dialog.Panel>
-          </motion.div>
-        </div>
-      </div>
+      >
+        <motion.div
+          initial={{ opacity: 0, scale: 0.95 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0.95 }}
+          transition={{ duration: 0.3 }}
+          className="flex h-full  items-center justify-center p-4 text-center  max-w-full"
+        >
+          <Dialog.Panel className="flex-1  h-full overflow-y-auto slimscroll w-full transform bg-white dark:bg-muted-800 p-6 lg:p-8 text-left align-middle shadow-xl">
+            <div className="mt-2 flex justify-center items-center min-h-[80vh] min-w-[95%] md:min-w-[75%] ">
+              {loading ? (
+                <div className="flex justify-center items-center w-full h-32">
+                  加载中...
+                </div>
+              ) : (
+                children
+              )}
+            </div>
+          </Dialog.Panel>
+          <div className="absolute right-8 top-8">
+            <BaseButtonClose
+              type="button"
+              onClick={onClose}
+            />
+          </div>
+        </motion.div>
+      </motion.div>
     </Dialog>
   );
 }; 
