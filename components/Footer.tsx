19d03e042e1ebcaae60bf5eb42e175726a3f0c5b
huayemao <huayemao4g@gmail.com>
2025-08-19 21:44:27 +0800

    feat: footer 新增导航链接

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,93 @@ import { SITE_META } from "@/constants";
 
 export function Footer() {
   return (
-    <div className="relative z-20 bg-muted-100 dark:bg-muted-900 text-muted-500 mt-auto">
-      <div className="w-full max-w-6xl mx-auto xl:max-w-[85%] py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+    <div className="relative z-20 bg-white dark:bg-muted-800 text-muted-500 mt-auto">
+      <div className="relative w-full max-w-7xl px-5 py-24 mx-auto z-20">
+        <div className="flex flex-col ptablet:flex-row ltablet:flex-row lg:flex-row gap-6 ptablet:gap-0 ltablet:gap-0 lg:gap-0 flex-wrap md:text-left text-center -mb-10 -mx-4">
+          <div  className="w-full ptablet:w-1/2 ltablet:w-1/4 lg:w-1/4 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left px-4">
+            <a  className="flex flex-col md:flex-row gap-3 title-font font-medium items-center md:justify-start justify-center text-muted-800 dark:text-white">
+              <img  className="h-12" src="/favicon.svg" alt="dors logo" width="48" height="48" />
+              <span  className="font-heading font-bold text-2xl">Dors</span>
+            </a>
+            <p  className="font-sans text-sm w-full max-w-xs mx-auto md:max-w-[220px] md:mx-0 mt-2 text-muted-500 dark:text-muted-400">
+              Dors 是花野猫开发为知识工作者打造的数字花园应用，包含的博客、个人记事本、及其他实用功能。
+            </p>
+          </div>
+          <div className="w-full ptablet:w-1/2 ltablet:w-1/4 lg:w-1/4 px-4">
+            <h2 className="font-heading font-medium text-muted-800 dark:text-white tracking-widest text-sm mb-3">
+              实用工具
+            </h2>
+            <ul className="font-sans list-none space-y-2 mb-10">
+              <li>
+                <a href="/notes" className="text-muted-600 dark:text-muted-400 hover:text-primary-500 dark:hover:text-primary-500">
+                  小记
+                </a>
+              </li>
+              <li>
+                <a href="/video-splitter" className="text-muted-600 dark:text-muted-400 hover:text-primary-500 dark:hover:text-primary-500">
+                  视频分割器
+                </a>
+              </li>
+              <li>
+                <a href="/quotes" className="text-muted-600 dark:text-muted-400 hover:text-primary-500 dark:hover:text-primary-500">
+                  影墨留香
+                </a>
+              </li>
+              <li>
+                <a href="/excel-renamer" className="text-muted-600 dark:text-muted-400 hover:text-primary-500 dark:hover:text-primary-500">
+                  excel 重命名工具
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="w-full ptablet:w-1/2 ltablet:w-1/4 lg:w-1/4 px-4">
+            <h2 className="font-heading font-medium text-muted-800 dark:text-white tracking-widest text-sm mb-3">
+              分类
+            </h2>
+            <ul className="font-sans list-none space-y-2 mb-10">
+              <li>
+                <a href="/categories/5" className="text-muted-600 dark:text-muted-400 hover:text-primary-500 dark:hover:text-primary-500">
+                  应试
+                </a>
+              </li>
+              <li>
+                <a href="/categories/9" className="text-muted-600 dark:text-muted-400 hover:text-primary-500 dark:hover:text-primary-500">
+                  效率
+                </a>
+              </li>
+              <li>
+                <a href="/categories/1" className="text-muted-600 dark:text-muted-400 hover:text-primary-500 dark:hover:text-primary-500">
+                  生活
+                </a>
+              </li>
+              <li>
+                <a href="/categories/11" className="text-muted-600 dark:text-muted-400 hover:text-primary-500 dark:hover:text-primary-500">
+                  文艺
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="w-full ptablet:w-1/2 ltablet:w-1/4 lg:w-1/4 px-4">
+            <h2 className="font-heading font-medium text-muted-800 dark:text-white tracking-widest text-sm mb-3">
+              创作
+            </h2>
+            <ul className="font-sans list-none space-y-2 mb-10">
+              <li>
+                <a href="/aigc" className="text-muted-600 dark:text-muted-400 hover:text-primary-500 dark:hover:text-primary-500">
+                  aigc
+                </a>
+              </li>
+              <li>
+                <a href="/diaries" className="text-muted-600 dark:text-muted-400 hover:text-primary-500 dark:hover:text-primary-500">
+                  月度随记
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted-100 dark:bg-muted-900 w-full  py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+        <div className="max-w-6xl mx-auto  xl:max-w-[85%]">
         <p className=" text-sm text-center sm:text-left">
           © 2022 - present. All Rights Reserved.
         </p>
@@ -19,6 +104,7 @@ export function Footer() {
             </a>
           </span>
         )}
+      </div>
       </div>
     </div>
   );
