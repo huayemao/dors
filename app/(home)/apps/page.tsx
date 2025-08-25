b965380e311106d4999f962acfbfa12bcf28d630
huayemao <huayemao4g@gmail.com>
2025-08-25 20:31:04 +0800

    fix: 修复应用台样式

--- app/(home)/apps/page.tsx
+++ app/(home)/apps/page.tsx
@@ -3,58 +3,54 @@ import { Application } from "@/components/Application";
 import { ClearCacheButton } from "@/components/ClearCacheButton";
 import { Metadata } from "next";
 
-
 export const revalidate = 1200;
 
 export const metadata: Metadata = {
-    title: '工作台',
-    description: '花野猫先生辛勤劳作之地',
+  title: "工作台",
+  description: "花野猫先生辛勤劳作之地",
 };
-
 
 //https://beta.nextjs.org/docs/data-fetching/fetching#segment-cache-configuration
 
-export default async function Apps({
-}: {
-    }) {
-    const resourceItems = await getResourceItems();
+export default async function Apps({}: {}) {
+  const resourceItems = await getResourceItems();
 
-    const apps = [
-        {
-            name: "小记",
-            href: "/notes",
-            iconName: "notebook",
-        },
-        {
-            name: "自留地",
-            href: "/protected",
-            iconName: "globe-lock",
-        },
-        {
-            name: "后台",
-            href: "/admin",
-            iconName: "cog",
-        },
-    ].concat(
-        resourceItems.map((e) => ({
-            name: e.title,
-            href: e.url,
-            iconName: e.iconName || "link",
-        }))
-    );
+  const apps = [
+    {
+      name: "小记",
+      href: "/notes",
+      iconName: "notebook",
+    },
+    {
+      name: "自留地",
+      href: "/protected",
+      iconName: "globe-lock",
+    },
+    {
+      name: "后台",
+      href: "/admin",
+      iconName: "cog",
+    },
+  ].concat(
+    resourceItems.map((e) => ({
+      name: e.title,
+      href: e.url,
+      iconName: e.iconName || "link",
+    }))
+  );
 
-    return (
-        <div className="flex flex-col gap-4">
-            <div className="flex justify-end">
-                <ClearCacheButton path="/apps" />
-            </div>
-            <div className="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-6 pb-8">
-                {apps.map((app, i) => (
-                    <Application key={i} {...app}></Application>
-                ))}
-            </div>
+  return (
+    <section className="flex flex-col gap-4  bg-muted-100 dark:bg-muted-800 pt-16 h-screen">
+      <div className="p-8 container mx-auto min-h-96">
+        <div className="flex justify-end ">
+          <ClearCacheButton path="/apps" />
         </div>
-    );
+        <div className="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-6 pb-8">
+          {apps.map((app, i) => (
+            <Application key={i} {...app}></Application>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
-
-
